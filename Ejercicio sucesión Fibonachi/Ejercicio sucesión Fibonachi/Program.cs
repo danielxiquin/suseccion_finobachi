9ecce916864cb5664a8390de67bd5c987ac743db
danielxiquin <123830108+danielxiquin@users.noreply.github.com>
2023-03-16 19:20:49 -0600

    Add files via upload

---
+++ Ejercicio sucesión Fibonachi/Ejercicio sucesión Fibonachi/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_sucesión_Fibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int A = 0;
+            int B = 1;
+            int C = 0;
+            int i = 2;
+            bool repetir =true;
+
+            
+
+            while (repetir == true)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese un numero");
+                    num = Convert.ToInt32(Console.ReadLine());
+                    if (num > 0)
+                    {
+                        
+                        Console.WriteLine("Resultado=" + A);
+
+                        if (num > 1)
+                        {
+                            
+                            Console.WriteLine("Resultado+=" + B);
+                            if (i < num)
+                            {
+                                C = A + B;
+                                
+                                Console.WriteLine("Resultado +=" + C);
+                                A = B;
+                                B = C;
+                                i = i++;
+
+                            }
+
+                        }
+                    }
+
+                }
+                catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                
+                }
+
+                
+            }
+            Console.WriteLine("Ingrese un numero");
+            num = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
